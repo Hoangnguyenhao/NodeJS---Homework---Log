91bc6c03105dde750fd33fbefa670316676db6c1
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-06-19 20:15:22 +0700

    Update db.js

--- Homework - Log/config/db.js
+++ Homework - Log/config/db.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose');
-const logger = require('../middlewares/logger');
+const mongoose = require('mongoose')
 
-const connectDB = async () => {
+async function connectDB() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/garage', {
+    await mongoose.connect('mongodb://127.0.0.1:27017/carDB', {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    });
-    logger.info('Connected to MongoDB successfully');
+    })
+    console.log('MongoDB connected')
   } catch (err) {
-    logger.error(`MongoDB connection error: ${err.message}`);
-    process.exit(1);
+    console.error('MongoDB error:', err.message)
+    process.exit(1)
   }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
